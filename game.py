0c4a79b3ee67ca1d6262bb8c8cf287b479dd032a
Mohamed El Saadany <76630212+mohamed-3090@users.noreply.github.com>
2023-05-20 22:02:45 +0300

    Add files via upload

--- game.py
+++ game.py
@@ -13,8 +13,8 @@ def main():
     prolog = Prolog()
 
     # Load the Prolog code file
-    prolog.consult("Connect4 minMax.pl")
-    time.sleep(5)
+    prolog.consult("alpha_beta.pl")
+    time.sleep(4)
     game_end = False
     while not game_end:
         (game_board, game_end) = board.get_game_grid()
@@ -22,7 +22,7 @@ def main():
         board.print_grid(game_board)
         transBoard = board._transpose_grid(game_board)
         progrid = board.boardToProlog(transBoard)
-        colomn = list(prolog.query("minMax(" + progrid + ", Col, r)"))[0]["Col"]
+        colomn = list(prolog.query("alpha_beta(" + progrid + ", r, Column)"))[0]["Column"]
         # YOUR CODE GOES HERE
 
         # Insert here the action you want to perform based on the output of the algorithm
@@ -32,7 +32,7 @@ def main():
             break
         board.select_column(colomn)
 
-        time.sleep(2)
+        time.sleep(4)
 
 
 if __name__ == "__main__":
